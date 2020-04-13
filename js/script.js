5609b9b24df5dba0dd012065e7efbf841b5941a8
Brian Mwiruki <mwiruki@outlook.com>
2020-04-13 14:55:25 +0300

    deployment to github

--- js/script.js
+++ js/script.js
@@ -5,3 +5,49 @@ spans.forEach(span => span.addEventListener('mouseover', function(e){
 spans.forEach(span => span.addEventListener('mouseout', function(e){
   span.classList.remove('animated', 'rubberBand')
 }))
+
+const htmlBar = document.querySelector('.bar-html')
+const cssBar = document.querySelector('.bar-css')
+const jsBar = document.querySelector('.bar-javascript')
+const angularBar = document.querySelector('.bar-angular')
+const pythonBar = document.querySelector('.bar-python')
+
+var t1 = new TimelineLite()
+
+
+t1.fromTo(htmlBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
+    .fromTo(cssBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
+    .fromTo(jsBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(75% - 6px)`, ease: Power4.easeOut})
+    .fromTo(angularBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(70% - 6px)`, ease: Power4.easeOut})
+    .fromTo(pythonBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(72% - 6px)`, ease: Power4.easeOut})
+
+
+const controller = new ScrollMagic.Controller()
+const scene = new ScrollMagic.Scene({
+  triggerElement: '.skills',
+  triggerHook: 0
+})
+.setTween(t1)
+.addTo(controller)
+
+const showRequiredCategory = event => {
+  const getId = event.id
+  const links = document.querySelectorAll('.work-category button')
+  for(i=0; i<links.length; i++){
+    if(links[i].hasAttribute('class')){
+      links[i].classList.remove('active')
+    }
+  }
+  event.classList.add('active')
+  const getCategory = document.querySelector(`.category-${getId}`)
+  const categories = document.querySelectorAll('div[class ^= "category-"]')
+  for(i=0; i<categories.length; i++){
+    if(categories[i].hasAttribute('class')){
+      categories[i].classList.remove('showCategory')
+      categories[i].classList.addd('hideCategory')
+
+    }
+  }
+  getCategory.classList.remove('hideCategory')
+  getCategory.classList.add('showCategory')
+}
